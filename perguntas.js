8428967a7f001ab59da641f6cf5320b5b02ba989
fazzim1509 <goncalves.silva.fabricio@escola.pr.gov.br>
2025-06-05 11:02:25 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,43 @@
+criaCartao(
+    'Robotica',
+    'O que é um resistor?',
+    'É  um componente eletrónico passivo que restringe a passagem de corrente elétrica em um circuito'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Matemática1',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Matematica',
+    'Qual é o fatorial de 10?',
+    '10!= 3628800'
+)
+criaCartao(
+    'Biologia',
+    'Qual a especie do escorpião?:',
+    'O escorpião e um Aracnideo'
+)
+criaCartao(
+    'Educação Financeira',
+    'Qual o valor do dolar nos dias de hj?',
+    'O dolar hj esta 5,60 reias'
+)
+criaCartao(
+    'Biotecnologia',
+    'Como se diz i em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
